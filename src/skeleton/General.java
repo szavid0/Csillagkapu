7a85c0971720aa2ea33e1f99b0679ff318b1a189
David Szanto <szavid0@gmail.com>
2016-03-27 21:50:18 +0200

    General methods (half-empty)

--- src/skeleton/General.java
+++ src/skeleton/General.java
@@ -1,18 +1,59 @@
+import java.util.ArrayList;
 
 public class General {
 	private AbstractBlock PosBlock;
 	private Direction direction;
+	private int lives;
+	private int ZPMCnt;
+	private int allZPMCnt;
+	private ArrayList<Box> boxes;
 	
+	public General(){
+		lives = 3;
+		ZPMCnt = 0;
+		boxes = new ArrayList<Box>();
+	}
+	
+	public AbstractBlock getPosBlock(){ return PosBlock;}
 	public void setDirection(Direction dir){
 		this.direction = dir;
+	}
+	public Direction getDirection(){
+		return direction;
 	}
 	public void setPosBlock(AbstractBlock block){
 		PosBlock = block;
 	}
-	public void die(){
+	public void collectZpm(Zpm modul){
 		//TODO
 	}
-	public void collectZpm(Zpm modul){
+	public void die(){
+		if(ZPMCnt == allZPMCnt){
+			//TODO
+			//Application endGame("WIN!")
+		}
+		else{
+			//TODO
+			//Application endGame("LOSE!")
+		}
+
+	}
+	public boolean hasBox(){
+		return boxes.size() > 0;
+	}
+	public Box getBox(){
+		return boxes.get(boxes.size() - 1);
+	}
+	public void move(Direction dir){
+		//TODO
+	}
+	public void pick(){
+		//TODO
+	}
+	public void drop(){
+		//TODO
+	}
+	public void shoot(Color col){
 		//TODO
 	}
 }
