09efd540e7d764c8d187593061e943347d1b0442
gkate90 <mirates90@gmail.com>
2016-03-28 22:43:32 +0200

    kommentelve

--- src/skeleton/Scale.java
+++ src/skeleton/Scale.java
@@ -1,10 +1,15 @@
-
+//Mérleg osztály megvalósítása, a mezőből öröklődik.
 public class Scale extends Field {
+
+	//A mérleghez tartozó ajtó.
 	private Door doorToOpen;
 	
+	//Konstruktor, doorToOpen-t állítja be.
 	public Scale(Door d){
 		this.doorToOpen=d;
 	}
+	
+	//Az ezredes rálép, ha nincs rajta doboz és összegyűjti a ZPM-et, ha van rajta. Ajtót nyit.
 	@Override
 	public void moveToThisBlock(){
 		if(containsBox == false){
