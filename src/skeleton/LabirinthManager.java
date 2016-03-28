53905dc04227925fe543f21dde4960c800b95d71
gkate90 <mirates90@gmail.com>
2016-03-28 23:34:37 +0200

    Balázs módosításai. Tesztesetek.

--- src/skeleton/LabirinthManager.java
+++ src/skeleton/LabirinthManager.java
@@ -4,11 +4,173 @@ import java.util.ArrayList;
 
 public class LabirinthManager {
 	private List<AbstractBlock> map = new ArrayList<AbstractBlock>();
-	private boolean WormHoleExists;
+	private boolean WormHoleExists = false;
 	private StarGate yellowStarGate;
 	private StarGate blueStarGate;
-	private Field startField = new Field();
+	private Field startField;
 	
+	public LabirinthManager(int teszteset){
+		switch(teszteset){
+		case 1: init1();break;
+		case 2: init1();break; 
+		case 3: init3();break;
+		case 4: init4();break;
+		case 5: init5();break;
+		case 6: init6();break;
+		case 7: init7();break;
+		case 8: init7();break;
+		case 9: init9();break;
+		case 10: init10();break;
+		case 11: init11();break;
+		case 12: init12();break;
+		case 13: init4();break;
+		case 14: init4();break;
+		default:return;
+		}
+		Application.general= new General();
+		Application.general.setPosBlock(startField);
+	}
+	private void init12() {
+		int[] t = new int[4];
+		t[2]=1;
+		map.add(new Wall(0,t));
+		t = new int[4];
+		t[3]=0;
+		t[2]=2;
+		map.add(new Field(1,t));
+		t = new int[4];
+		t[3]=1;
+		t[2]=3;
+		startField = new Field(2,t);
+		map.add(startField);
+		t = new int[4];
+		t[3]=2;
+		yellowStarGate = new StarGate(3,t,Direction.WEST,Color.YELLOW);
+		map.add(yellowStarGate);
+	}
+	private void init11() {
+		int[] t = new int[4];
+		t[2]=1;
+		blueStarGate = new StarGate(0,t,Direction.EAST,Color.BLUE);
+		map.add(blueStarGate);
+		t = new int[4];
+		t[3]=0;
+		t[2]=2;
+		map.add(new Field(1,t));
+		t = new int[4];
+		t[3]=1;
+		startField = new Field(2,t);
+		map.add(startField);
+	}
+	private void init10() {
+		int[] t = new int[4];
+		t[2]=1;
+		map.add(new Wall(0,t));
+		t = new int[4];
+		t[3]=0;
+		t[2]=1;
+		map.add(new Field(1,t));
+		t = new int[4];
+		t[3]=1;
+		map.add(new Field(2,t));	
+		
+	}
+	private void init9() {
+		int[] t = new int[4];
+		t[2]=2;
+		yellowStarGate = new StarGate(0,t,Direction.EAST,Color.YELLOW,true);
+		map.add(yellowStarGate);
+		t = new int[4];
+		t[3]=1;
+		t[2]=3;
+		startField = new Field(1,t);
+		map.add(startField);
+		t = new int[4];
+		t[3]=2;
+		t[2]=4;
+		map.add(new Field(2,t));
+		t = new int[4];
+		t[3] = 3;
+		yellowStarGate = new StarGate(3,t,Direction.WEST,Color.BLUE,true);
+		map.add(blueStarGate);
+		WormHoleExists = true;
+	}
+
+	private void init7() {
+		ArrayList<Box> b = new ArrayList<Box>();
+		b.add(new Box());
+		Application.general = new General(b);
+		int[] t = new int[4];
+		t[2] = 2;
+		map.add(new Field(0,t));
+		t = new int[4];
+		t[3] = 0;
+		startField = new Field(1,t);
+		map.add(startField);
+	}
+	private void init6() {
+		int[] t = new int[4];
+		t[2] = 1;
+		startField=new Field(0,t);
+		map.add(startField);
+		t=new int[4];
+		t[3]=0;
+		map.add(new Field(1,t,true,false));
+	}
+	private void init5() {
+		int[] t = new int[4];
+		t[2] = 1;
+		Door d = new Door(0,t);
+		map.add(d);
+		t = new int[4];
+		t[2]=2;
+		t[3]=0;
+		map.add(new Scale(1,t,d,false,false));
+		t = new int[4];
+		t[3]=2;
+		startField = new Field(2,t);
+		map.add(startField);
+	}
+	private void init4() {
+		ArrayList<Box> b = new ArrayList<Box>();
+		b.add(new Box());
+		Application.general = new General(b);
+		int[] t = new int[4];
+		t[2] = 1;
+		Door d = new Door(0,t);
+		map.add(d);
+		t = new int[4];
+		t[2]=2;
+		t[3]=0;
+		map.add(new Scale(1,t,d,true,false));
+		t = new int[4];
+		t[3] = 1;
+		startField = new Field(2,t);
+		map.add(startField);
+	}
+	private void init3() {
+		int[] t = new int[4];
+		t[3] = 1;
+		map.add(new Field(0,t,false,true));
+		t = new int[4];
+		t[3] = 0;
+		startField = new Field(1,t);
+		map.add(startField);
+	}
+
+	private void init1() {
+		int[] t = new int[4];
+		t[2] = 1; 
+		map.add(new Canyon(0,t));
+		t = new int[4];
+		t[3] = 0;
+		startField = new Field(1,t);
+		map.add(startField);
+	}
+	public void setStarGate(StarGate s){
+		if(s.getColor()==Color.BLUE)blueStarGate = s;
+		else yellowStarGate = s;
+	}
 	private Direction oppDir(Direction dir){
 		if(dir == Direction.EAST)
 			dir = Direction.WEST;
@@ -30,11 +192,11 @@ public class LabirinthManager {
 		System.out.println("LabirinthMaganger.createStarGate(w, col, dir)");
 		
 		if(col == Color.BLUE){
-			blueStarGate = new StarGate(dir, Color.BLUE, w.getIndex());
+			blueStarGate = new StarGate(w.getIndex(),w.getNeighboursIndex(),dir, Color.BLUE);
 			map.add(w.getIndex(), blueStarGate);
 		}
 		else{
-			yellowStarGate = new StarGate(dir, Color.YELLOW, w.getIndex());
+			yellowStarGate = new StarGate(w.getIndex(),w.getNeighboursIndex(),dir, Color.YELLOW);
 			map.add(w.getIndex(), yellowStarGate);
 		}
 		
@@ -43,10 +205,10 @@ public class LabirinthManager {
 		System.out.println("LabirinthMaganger.deleteStarGate(col)");
 		
 		if(col == Color.BLUE){
-			map.add(blueStarGate.getIndex(), new Wall(true, blueStarGate.getIndex()));
+			map.set(blueStarGate.getIndex(), new Wall(blueStarGate.getIndex(),blueStarGate.getNeighboursIndex(),true));
 		}
 		else{
-			map.add(yellowStarGate.getIndex(), new Wall(true, yellowStarGate.getIndex()));
+			map.set(yellowStarGate.getIndex(), new Wall(blueStarGate.getIndex(),blueStarGate.getNeighboursIndex(),true));
 		}
 	}
 	public StarGate getYellowStarGate(){
